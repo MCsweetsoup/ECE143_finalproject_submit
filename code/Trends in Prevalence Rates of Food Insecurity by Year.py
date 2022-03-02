93436efc5ca89d77c7e6997384731e5fccfaf49e
Krystal Chan <krystalchaneleven@gmail.com>
2022-03-01 19:53:26 -0800

    Updated the Lines

--- code/Trends in Prevalence Rates of Food Insecurity by Year.py
+++ code/Trends in Prevalence Rates of Food Insecurity by Year.py
@@ -11,10 +11,10 @@ data = pd.read_excel('Trends in Prevalence Rates of Food Insecurity by Year.xlsx
 plt.figure(figsize=(7,7))
 
 #Food Insecure People Plot
-plt.plot(data['Food insecurity '], label = r'Food Insecure', c = '#f8c928')
+plt.plot(data['Food insecurity '], label = r'Food Insecure', c = '#f8c928', linewidth = 4)
 
 #Very low food security plot
-plt.plot(data['Very low food security'], label = r'Very Low Food Security', c = '#c35b7e')
+plt.plot(data['Very low food security'], label = r'Very Food Insecure', c = '#c35b7e', linewidth = 4)
 
 year = range(1995,2020)
 
@@ -27,17 +27,17 @@ plt.locator_params(axis="x", nbins=8)
 
 #mark important year line dates from 1995-2020
 plt.axvline(x = 14, color = 'black', ls = ':', alpha = 0.75)
-plt.annotate('2009 Great Recession', rotation = 90, xy = (14.2, 0.5), fontsize = 7)
+plt.annotate('2009 Great Recession', rotation = 90, xy = (14.2, 0.5), fontsize = 9)
 plt.axvline(x = 6, color = 'black', ls = ':',alpha = 0.75)
-plt.annotate('9/11 Terroists Attack', rotation = 90, xy = (6.2, 0.5), fontsize = 7)
+plt.annotate('9/11 Terroists Attack', rotation = 90, xy = (6.2, 0.5), fontsize = 9)
 plt.axvline(x = 24, color = 'black', ls = ':', alpha = 0.75)
-plt.annotate('COVID-19 Pandemic', rotation = 90, xy = (24.2, 0.5), fontsize = 7)
+plt.annotate('COVID-19 Pandemic', rotation = 90, xy = (24.2, 0.5), fontsize = 9)
 
 #mark important percentages
 plt.axhline(y = 12, color = 'red', ls = '--' ,alpha = 0.5 )
-plt.annotate('Severe Food Insecurity', xy = (0, 12.2), fontsize = 7, color = 'red')
+plt.annotate('Severe Food Insecurity', xy = (0, 12.2), fontsize = 9, color = 'red')
 plt.axhline(y = 5, color = 'red',ls = '--', alpha = 0.5)
-plt.annotate('Severe Very Food Insecure', xy = (0, 5.2), fontsize = 7, color = 'red')
+plt.annotate('Severe Very Food Insecure', xy = (0, 5.2), fontsize = 9, color = 'red')
 
 #place legend
 plt.legend(loc='upper right')
